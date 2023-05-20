16e79f9ea84266e02822303963ddbdf9de454915
ccg <ccgccg@yeah.net>
2023-05-20 17:46:04 +0800

    2023/5/20
    功能更新：对文章数据加入redis缓存，修改浏览量的更新逻辑，提升访问速度

--- Blog-framework/src/main/java/com/zxnk/job/ViewCountUpdateJob.java
+++ Blog-framework/src/main/java/com/zxnk/job/ViewCountUpdateJob.java
@@ -1,6 +1,7 @@
 package com.zxnk.job;
 
 import com.zxnk.service.ArticleService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,11 +13,13 @@ import java.util.concurrent.TimeUnit;
 /**
  * @ClassName ViewCountUpdateJob
  * @Description 博文浏览量定时更新任务
+ * 2023/5/20 完成一次功能更新，加入删除热点文章缓存数据和文章列表数据
  * @Author cc
  * @Date 2023/5/7 17:14
  * @Version 1.0
  */
 @Component
+@Slf4j
 public class ViewCountUpdateJob {
 
     @Autowired
@@ -24,11 +27,15 @@ public class ViewCountUpdateJob {
     @Autowired
     private ArticleService articleService;
 
-    //定时任务，每隔两分半中同步一次数据库，完成博文浏览量的更新
-    @Scheduled(cron = "30 2/2 * * * *")
+    //定时任务，每隔十分中同步一次数据库，完成博文浏览量的更新，并删除hotArticleList的缓存
+    @Scheduled(cron = "0 5/5 * * * *")
     public void updateViewCount(){
         Map<String,Integer> viewCount = redisTemplate.opsForHash().entries("viewCount");
         //完成数据库的更新
         articleService.updateArticles(viewCount);
+        //删除缓存
+        redisTemplate.delete("hotArticleList");
+        redisTemplate.delete("articleList");
+        log.info("完成浏览量更新和缓存删除");
     }
 }
